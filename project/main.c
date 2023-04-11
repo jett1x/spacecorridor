f0b054868ed9816cc19f19638c33c1deef5b4875
Denys <sh.denys.ev@gmail.com>
2023-04-11 21:39:29 +0200

    fixed everything

--- project/main.c
+++ project/main.c
@@ -1,21 +1,106 @@
-/// @file            main.c
-/// @brief           Programme principal initial du niveau 1
-/// @author          Mathieu Constant, Ivan Osadtsiv, Denys Scherba
-/// @version         1.0
-/// @date            10.04.2023
-
+/**
+ * \file            main.c
+ * \brief           Programme principal initial du niveau 1
+ * \author          Mathieu Constant, Ivan Osadtsiv, Denys Scherba
+ * \version         1.0
+ * \date            10.04.2023
+ */
 
 #include "sdl2-light.h"
 #include <stdio.h>
 
 
+/**
+ * \brief           Largeur de l'écran de jeu
+ */
+#define SCREEN_WIDTH 300
 
-/// @brief Initiallisation of sprite_t
-/// @param sprite 
-/// @param x 
-/// @param y 
-/// @param w 
-/// @param h 
+/**
+ * \brief           Hauteur de l'écran de jeu
+ */
+#define SCREEN_HEIGHT 480
+
+
+/**
+ * \brief           Taille d'un vaisseau
+ */
+
+#define SHIP_SIZE 32
+
+
+/**
+ * \brief           Taille d'un météorite
+*/
+
+#define METEORITE_SIZE 32
+
+
+/**
+ * \brief           Hauteur de la ligne d'arrivée
+ */
+
+
+#define FINISH_LINE_HEIGHT 10
+
+
+/**
+ * \brief           Pas de déplacement horizontal du vaisseau
+*/
+
+#define MOVING_STEP 10
+
+
+/**
+  * \brief          Vitesse initiale de déplacement vertical des éléments du jeu 
+*/
+
+#define INITIAL_SPEED 2
+
+
+/**
+ * \brief            Pas de changement de vitesse
+*/
+
+#define SPEED_DELTA 0.5
+
+
+/**
+ * \brief           Représentation pour stocker les textures nécessaires à l'affichage graphique et type qui correspond aux textures du jeu
+*/
+
+typedef struct textures_s
+{
+    SDL_Texture* background; /*!< Texture liée à l'image du fond de l'écran. */
+    SDL_Texture* ship;
+    SDL_Texture* line;
+    SDL_Texture* meteorite;
+}textures_t;
+
+/**
+ * \brief           Type de sprite
+*/
+typedef struct sprite_s
+{
+    int x;
+    int y;
+    int h;
+    int w;
+} sprite_t;
+
+/**
+ * \brief           Type qui correspond aux données du monde
+ */
+
+typedef struct world_s
+{
+    int gameover; /*!< Champ indiquant si l'on est à la fin du jeu */
+    float vy;       /*!< A field indicate a vertical speed */
+    sprite_t * ship;
+    sprite_t * line;
+    sprite_t * wall;
+}world_t;
+
+
 void init_sprite(sprite_t *sprite, int x, int y, int w, int h)
 {
     sprite->x = x;
@@ -24,9 +109,19 @@ void init_sprite(sprite_t *sprite, int x, int y, int w, int h)
     sprite->h = h;
 }
 
+/// @brief Writes to console all the data correspond to particular sprite
+/// @param sprite a sprite you want to get information about
+void print_sprite(sprite_t *sprite)
+{
+    printf("Coordinates of sprite are x:%d y:%d\n", sprite->x, sprite->y);
+    printf("The size of sprite is H:%d W:%d\n", sprite->h, sprite->w);
+}
 
-/// @brief           La fonction initialise les données du monde du jeu
-/// @param world     les données du monde
+/**
+ * \brief           La fonction initialise les données du monde du jeu
+ * \param world     les données du monde
+ */
+
 void init_data(world_t * world)
 {
     //on n'est pas à la fin du jeu
@@ -45,9 +140,103 @@ void init_data(world_t * world)
 }
 
 
-/// @brief           La fonction initialise les textures nécessaires à l'affichage graphique du jeu
-/// @param screen    la surface correspondant à l'écran de jeu
-/// @param textures  les textures du jeu
+/**
+ * \brief           La fonction nettoie les données du monde
+ * \param world     les données du monde
+ */
+
+
+void clean_data(world_t *world){
+    /* utile uniquement si vous avez fait de l'allocation dynamique (malloc); la fonction ici doit permettre de libérer la mémoire (free) */
+    
+}
+
+
+
+/**
+ * \brief           La fonction indique si le jeu est fini en fonction des données du monde
+ * \param world     les données du monde
+ * \return          1 si le jeu est fini, 0 sinon
+ */
+
+int is_game_over(world_t *world)
+{
+    return world->gameover;
+}
+
+
+
+/**
+ * \brief           La fonction met à jour les données en tenant compte de la physique du monde
+ * \param les       données du monde
+ */
+
+void update_data(world_t *world)
+{
+    world->line->y += world->vy;
+    world->wall->y += world->vy;
+}
+
+
+
+/**
+ * \brief           La fonction gère les évènements ayant eu lieu et qui n'ont pas encore été traités
+ * \param event     paramètre qui contient les événements
+ * \param world     les données du monde
+ */
+
+void handle_events(SDL_Event *event,world_t *world)
+{
+    Uint8 *keystates;
+    while( SDL_PollEvent( event ) ) 
+    {
+        
+        //Si l'utilisateur a cliqué sur le X de la fenêtre
+        if( event->type == SDL_QUIT ) 
+        {
+            //On indique la fin du jeu
+            world->gameover = 1;
+        }
+       
+         //si une touche est appuyée
+         if(event->type == SDL_KEYDOWN){
+            switch (event->key.keysym.sym)
+            {
+            //si la touche appuyée est 'D'
+            case SDLK_d:
+                printf("La touche D est appuyée\n");
+                break;
+            // si la touche appuyée est 'RIGHT'
+            case SDLK_RIGHT:
+                (world->ship->x) += MOVING_STEP;
+                break;
+            // si la touche appuyée est 'LEFT'
+            case SDLK_LEFT:
+                (world->ship->x) -= MOVING_STEP;
+                break;
+            // si la touche appuyée est 'ESC'
+            case SDLK_ESCAPE:
+                world->gameover = 1;
+                break;
+            // si la touche appuyée est 'DOWN'
+            case SDLK_DOWN:
+                (world->vy) -= SPEED_DELTA;
+                break;
+            // si la touche appuyée est 'UP'
+            case SDLK_UP:
+                (world->vy) += SPEED_DELTA;
+                break;
+            }
+        }
+    }
+}
+
+/**
+ * \brief           La fonction initialise les textures nécessaires à l'affichage graphique du jeu
+ * \param screen    la surface correspondant à l'écran de jeu
+ * \param textures  les textures du jeu
+*/
+
 void  init_textures(SDL_Renderer *renderer, textures_t *textures)
 {
     textures->background = load_image( "ressources/space-background.bmp", renderer);
@@ -56,21 +245,117 @@ void  init_textures(SDL_Renderer *renderer, textures_t *textures)
     textures->meteorite = load_image( "ressources/meteorite.bmp", renderer);
 }
 
+/**
+ * \brief           La fonction nettoie les textures
+ * \param textures  les textures
+*/
 
-/// @brief           fonction qui initialise le jeu: initialisation de la partie graphique (SDL), chargement des textures, initialisation des données
-/// @param window    la fenêtre du jeu
-/// @param renderer  le renderer
-/// @param textures  les textures
-/// @param world     le monde
-void init(SDL_Window **window, SDL_Renderer ** renderer, textures_t *textures, world_t * world)
+void clean_textures(textures_t *textures)
 {
+    clean_texture(textures->background);
+    clean_texture(textures->ship);
+    clean_texture(textures->line);
+    clean_texture(textures->meteorite);
+}
+
+/**
+ * \brief           La fonction applique la texture du fond sur le renderer lié à l'écran de jeu
+ * \param renderer  le renderer
+ * \param texture   la texture liée au fond
+*/
+
+void apply_background(SDL_Renderer *renderer, SDL_Texture *texture)
+{
+    if(texture != NULL){
+      apply_texture(texture, renderer, 0, 0);
+    }
+}
+
+/// @brief          Applies sprite on a screen
+/// @param renderer renderer
+/// @param texture  texture
+/// @param sprite   sprite to apply
+void apply_sprite(SDL_Renderer *renderer, SDL_Texture *texture, sprite_t *sprite)
+{
+    apply_texture(texture, renderer, sprite->x, sprite->y);
+}
+
+/// @brief          Applies a wall of sprites on a screen
+/// @param renderer renderer
+/// @param texture  texture
+/// @param sprite   sprite to apply
+void apply_wall(SDL_Renderer *renderer, SDL_Texture *texture, sprite_t *sprite)
+{
+    for(size_t i = 0; i < 3; i++)
+    {
+        for(size_t j = 0; j < 7; j++)
+        {
+            apply_texture(texture, renderer, sprite->x + METEORITE_SIZE*i, sprite->y + METEORITE_SIZE*j); 
+        }
+    }
+}
+
+/**
+ * \brief           La fonction rafraichit l'écran en fonction de l'état des données du monde
+ * \param renderer  le renderer lié à l'écran de jeu
+ * \param world     les données du monde
+ * \param textures  les textures
+ */
+
+void refresh_graphics(SDL_Renderer *renderer, world_t *world,textures_t *textures){
+    
+    //on vide le renderer
+    clear_renderer(renderer);
+    //application des textures dans le renderer
+    apply_background(renderer, textures->background);
+    //application de vaisseau texture dans le renderer
+    apply_sprite(renderer, textures->ship, world->ship);
+    //application de ligne texture dans le renderer
+    apply_sprite(renderer, textures->line, world->line);
+    //application de mur des meteorites dans le renderer
+    apply_wall(renderer, textures->meteorite, world->wall);
+    // on met à jour l'écran
+    update_screen(renderer);
+}
+
+
+
+/**
+* \brief            fonction qui nettoie le jeu: nettoyage de la partie graphique (SDL), nettoyage des textures, nettoyage des données
+* \param window     la fenêtre du jeu
+* \param renderer   le renderer
+* \param textures   les textures
+* \param world      le monde
+*/
+
+void clean(SDL_Window *window, SDL_Renderer * renderer, textures_t *textures, world_t * world){
+    clean_data(world);
+    clean_textures(textures);
+    clean_sdl(renderer,window);
+}
+
+
+
+/**
+ * \brief           fonction qui initialise le jeu: initialisation de la partie graphique (SDL), chargement des textures, initialisation des données
+ * \param window    la fenêtre du jeu
+ * \param renderer  le renderer
+ * \param textures  les textures
+ * \param world     le monde
+ */
+
+void init(SDL_Window **window, SDL_Renderer ** renderer, textures_t *textures, world_t * world){
     init_sdl(window,renderer,SCREEN_WIDTH, SCREEN_HEIGHT);
     init_data(world);
     init_textures(*renderer,textures);
 }
 
 
-/// @brief          programme principal qui implémente la boucle du jeu
+/**
+ *  \brief          programme principal qui implémente la boucle du jeu
+ */
+
+
 int main( int argc, char* args[] )
 {
     SDL_Event event;
