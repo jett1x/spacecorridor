28f81bf8e9bfa238e8371cbdc24fbc8de041b991
OSADTSIV Ivan <ivan.osadtsiv8@etu.univ-lorraine.fr>
2023-04-13 17:10:30 +0200

    Update space_logics.c

--- project/space_logics.c
+++ project/space_logics.c
@@ -3,6 +3,8 @@
 #include "space_graphics.h"
 #include "sdl2-light.h"
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 int is_game_over(world_t *world)
 {
@@ -10,10 +12,42 @@ int is_game_over(world_t *world)
 }
 
 
-void update_data(world_t *world)
+bool sprites_collide(sprite_t *sp1, sprite_t *sp2)
+{
+    if((abs(sp1->x - sp2->x) <= (sp1->w + sp2->w)/2) && (abs(sp1->y - sp2->y) <= (sp1->h + sp2->h)/2))
+    {
+        printf("COLLISION\n");
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+void handle_sprites_collision(sprite_t *sp1, sprite_t *sp2, world_t *world, SDL_Texture *texture1)
+{
+    bool make_dissapear = 0;
+    if(sprites_collide(sp1, sp2))
+    {
+        world->vy = 0;
+        make_dissapear = 1;
+    }
+
+    if(make_dissapear)
+    {
+        clean_texture(texture1);
+    }
+}
+
+
+void update_data(world_t *world, textures_t *textures)
 {
     world->line->y += world->vy;
     world->wall->y += world->vy;
+
+    handle_sprites_collision(world->ship, world->wall, world, textures->ship);
 }
 
 
