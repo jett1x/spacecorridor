afa3d14e7ef227c74a7787eac05fbdd92ad971ae
Denys <sh.denys.ev@gmail.com>
2023-04-13 18:14:08 +0200

    replaced main by refactored version

---
+++ project/space_graphics.h
@@ -0,0 +1,56 @@
+/// @file           space_graphics.h
+/// @author         OSADTSIV Ivan, SCHERBA Denys
+/// @brief          TP2
+
+#ifndef SPACE_GRAPHICS_H
+#define SPACE_GRAPHICS_H
+
+#include "sdl2-light.h"
+#include "space_logics.h"
+
+
+/// @brief          Writes to console all the data correspond to particular sprite
+/// @param sprite   a sprite you want to get information about
+void print_sprite(sprite_t *sprite);
+
+
+/// @brief          La fonction nettoie les textures
+/// @param textures les textures
+void clean_textures(textures_t *textures);
+
+
+/// @brief          La fonction applique la texture du fond sur le renderer lié à l'écran de jeu
+/// @param renderer le renderer
+/// @param texture  la texture liée au fond
+void apply_background(SDL_Renderer *renderer, SDL_Texture *texture);
+
+
+/// @brief          Applies sprite on a screen
+/// @param renderer renderer
+/// @param texture  texture
+/// @param sprite   sprite to apply
+void apply_sprite(SDL_Renderer *renderer, SDL_Texture *texture, sprite_t *sprite);
+
+
+/// @brief          Applies a wall of sprites on a screen
+/// @param renderer renderer
+/// @param texture  texture
+/// @param sprite   sprite to apply
+void apply_wall(SDL_Renderer *renderer, SDL_Texture *texture, sprite_t *sprite);
+
+
+/// @brief          La fonction rafraichit l'écran en fonction de l'état des données du monde
+/// @param renderer le renderer lié à l'écran de jeu
+/// @param world    les données du monde
+/// @param textures les textures
+void refresh_graphics(SDL_Renderer *renderer, world_t *world,textures_t *textures);
+
+
+/// @brief          fonction qui nettoie le jeu: nettoyage de la partie graphique (SDL), nettoyage des textures, nettoyage des données
+/// @param window   la fenêtre du jeu
+/// @param renderer le renderer
+/// @param textures les textures
+/// @param world    le monde
+void clean(SDL_Window *window, SDL_Renderer * renderer, textures_t *textures, world_t * world);
+
+#endif
---
+++ project/space_logics.h
@@ -0,0 +1,80 @@
+/// @file           space_logics.h
+/// @author         OSADTSIV Ivan, SCHERBA Denys
+/// @brief          TP2
+
+#include "sdl2-light.h"
+#include "space_graphics.h"
+#include "space_graphics.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#ifndef SPACE_LOGICS_H
+#define SPACE_LOGICS_H
+
+/// @brief          Représentation pour stocker les textures nécessaires à l'affichage graphique et type qui correspond aux textures du jeu
+typedef struct textures_s
+{
+    SDL_Texture* background; /*!< Texture liée à l'image du fond de l'écran. */
+    SDL_Texture* ship;
+    SDL_Texture* line;
+    SDL_Texture* meteorite;
+} textures_t;
+
+/// @brief           Type de sprite
+typedef struct sprite_s
+{
+    int x;
+    int y;
+    int h;
+    int w;
+} sprite_t;
+
+/// @brief           Type qui correspond aux données du monde
+typedef struct world_s
+{
+    int gameover; /*!< Champ indiquant si l'on est à la fin du jeu */
+    float vy;       /*!< A field indicate a vertical speed */
+    sprite_t * ship;
+    sprite_t * line;
+    sprite_t * wall;
+} world_t;
+
+
+/// @brief          La fonction indique si le jeu est fini en fonction des données du monde
+/// @param world    les données du monde
+/// @return         1 si le jeu est fini, 0 sinon
+int is_game_over(world_t *world);
+
+
+/// @brief     checks if sprites collide
+/// @param sp1 sprite 1
+/// @param sp2 sprite 2
+/// @return    1 if sprites collide, else 0
+bool sprites_collide(sprite_t *sp1, sprite_t *sp2);
+
+
+/// @brief          if sprites collide, resets vertical velocity to 0
+/// @param sp1      sprite 1
+/// @param sp2      sprite 2
+/// @param world    world
+/// @param texture1 texture of the first sprite
+void handle_sprites_collision(sprite_t *sp1, sprite_t *sp2, world_t *world, SDL_Texture *texture1);
+
+
+/// @brief          La fonction met à jour les données en tenant compte de la physique du monde
+/// @param les      données du monde
+void update_data(world_t *world, textures_t *textures);
+
+
+/// @brie           La fonction nettoie les données du monde
+/// @param world    les données du monde
+void clean_data(world_t *world);
+
+
+/// @brief          La fonction gère les évènements ayant eu lieu et qui n'ont pas encore été traités
+/// @param event    paramètre qui contient les événements
+/// @param world    les données du monde
+void handle_events(SDL_Event *event,world_t *world);
+
+#endif
