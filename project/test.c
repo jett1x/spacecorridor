fa8c84b8004fb6d513ca4d0e3afefb1dc8feae91
OSADTSIV Ivan <ivan.osadtsiv8@etu.univ-lorraine.fr>
2023-05-16 22:29:38 +0200

    test.c(not working)

--- project/test.c
+++ project/test.c
@@ -8,6 +8,12 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+/// @brief          Initiallisation of sprite_t
+/// @param sprite   sprite
+/// @param x        x coordinate
+/// @param y        y coordinate
+/// @param w        width
+/// @param h        heigth
 void init_sprite(sprite_t *sprite, int x, int y, int w, int h)
 {
     sprite->x = x;
@@ -19,14 +25,16 @@ void init_sprite(sprite_t *sprite, int x, int y, int w, int h)
 void test_init_sprite_param()
 {
     sprite_t * test;
-    int val = 1;
-    init_sprite(test, val, val, val, val);
+    int val1 = 1;
+    int val2 = 50;
+    init_sprite(test, val1, val1, val2, val2);
     print_sprite(test);
 }
 
-
+ 
 int main(int argc, char * argv[]) 
 {
+
     test_init_sprite_param();
-    return EXIT_SUCCESS;
+    return 0;
 }
